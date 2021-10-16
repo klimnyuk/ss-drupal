9dbfc8d66967804a3390512bfa75338e63fffaea
Aleksey Klimnyuk <aleksey.klimnyuk@gmail.com>
2021-10-16 21:25:51 +0300

    jenkins file and terraform file updated

--- terraform.tf
+++ terraform.tf
@@ -5,17 +5,25 @@ region = "eu-central-1"
 resource "aws_instance" "my_ubuntu" {
 ami = "ami-05f7491af5eef733a"
 instance_type = "t2.micro"
+key_name = "test"
 vpc_security_group_ids = [aws_security_group.my_webserver.id]
 user_data = <<EOF
 #!/bin/bash
-apt-get update -y
-apt-get upgrade -y
-apt-get install -y apache2
-service apache2 start
+wget -q \
+https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.4.9-1_amd64.deb \
+https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_20.10.9~3-0~ubuntu-focal_amd64.deb \
+https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_20.10.9~3-0~ubuntu-focal_amd64.deb
+dpkg -i *.deb
+usermod -aG docker ubuntu
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 EOF
 
 tags = {
   Name = "Web Server"
+}
+provisioner "local-exec" {
+  command = "echo ${self.public_ip} > public_ip"
 }
 }
 
@@ -25,6 +33,20 @@ resource "aws_security_group" "my_webserver" {
   ingress {
     from_port = 80
     to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
